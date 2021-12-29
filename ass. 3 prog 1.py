db33465b7c90892872fd7724af8cfca54e0a0e4f
shwetal14 <95999701+shwetal14@users.noreply.github.com>
2021-12-30 00:13:35 +0530

    Add files via upload

---
+++ ass. 3 prog 1.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[38]:
+
+
+
+lst = []
+total = int(input('Total numbers in List: '))
+
+for num in range(total):
+    num = int(input('Enter number '))
+    lst.append(num)
+
+print("Sum of elements in given list is :", sum(lst))
+
+
+# In[ ]:
+
+
+
+
